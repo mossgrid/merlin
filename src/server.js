e971092aa8adc222483ccd6ee79656738ebd30e6
sijibomi <gbemilesanmi@gmail.com>
2023-08-04 04:11:40 +0100

    server opts

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-import express from 'express'
-import { awakenMerlin } from './index.js'
-const app = express()
-const PORT = process.env.PORT || 3000
+import express from 'express';
+import { awakenMerlin } from './index.js';
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/merlin', (_req, res) => {
-    awakenMerlin()
-    res.send('Merlin is waking up...')
-})
+    awakenMerlin();
+    res.send('Merlin is waking up...');
+});
 
 app.listen(PORT, () => {
-    awakenMerlin()
-    console.log(`Server is up!`)
-})
+    awakenMerlin();
+    console.log(`Server is up!`);
+});
